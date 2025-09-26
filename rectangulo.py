9949cb96b3a662a445a7d3ed029c24be116f259c
Ismael Quesada Plata <ismaelqp27@gmail.com>
2025-09-26 14:06:56 +0200

    rectangulo

---
+++ rectangulo.py
@@ -0,0 +1,8 @@
+base=input("introduce la base ")
+altura=input("introduce la altura ")
+resultado=float(base)*float(altura)
+resultado2=float(resultado)+float(base)*2
+print("el area es ") 
+print(resultado)
+print("el perimetro es ") 
+print(resultado2)
