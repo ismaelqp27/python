6b51ff43925fbe8cc23da00e8f9396d0f1a8d194
Ismael Quesada Plata <ismaelqp27@gmail.com>
2025-10-09 10:41:19 +0200

    ejercicio 5

---
+++ ejercicio5.py
@@ -0,0 +1,4 @@
+horas = int (input ("¿cuantas horas has trabajado? "))
+dinero = int (input ("¿a cuanto te pagan la hora? "))
+multi = (horas * dinero)
+print ("el dinero que has ganado es" , multi)
