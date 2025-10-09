d1bda9db5fdcaa6b635cb2ab60d58a075d700a3a
Ismael Quesada Plata <ismaelqp27@gmail.com>
2025-10-09 10:05:08 +0200

    ejercicio 2

---
+++ imc.py
@@ -0,0 +1,7 @@
+peso = float(input("Introduce tu peso en kg: "))
+estatura = float(input("Introduce tu estatura en metros: "))
+
+imc = peso / (estatura ** 2)
+
+print ("Tu índice de masa corporal es ")
+print (imc)
