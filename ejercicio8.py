4cc425ccbe7a88057a84ea1a54f286cfc594a19e
Ismael Quesada Plata <ismaelqp27@gmail.com>
2025-10-23 09:56:38 +0200

    ejercicio8

---
+++ ejercicio8.py
@@ -0,0 +1,5 @@
+n = int(input("Introduce el primer número (n): "))
+m = int(input("Introduce el segundo número (m): "))
+c = n // m
+r = n % m
+print(f"{n} entre {m} da un cociente {c} y un resto {r}")
