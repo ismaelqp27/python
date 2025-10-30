c0f265022caeb5ef06574ebf40c907464bf37c3c
Ismael Quesada Plata <ismaelqp27@gmail.com>
2025-10-30 10:44:46 +0100

    ejercicio2 tercerpdf

---
+++ ejercicio2.py
@@ -0,0 +1,7 @@
+contraseña = ("contraseña")
+contraseñaprueba = (input ("escribe la contraseña "))
+contraseñaprueba = contraseñaprueba.lower ()
+if contraseña == contraseñaprueba:
+    print ("contrseña correcta")
+else:
+    print ("esa no es la contrseña")
