3a2bd9a1ebce3f4c1a4d6d05ae5a3cdcf395cabb
Ismael Quesada Plata <ismaelqp27@gmail.com>
2025-11-06 11:06:10 +0100

    ejercicio 10 tercer pdf

---
+++ ejercicio10.py
@@ -0,0 +1,5 @@
+pizza = input ("que pizza quieres (vegetariana o no vegetariana): ")
+if pizza == "vegetariana":
+    print ("los ingredientes para esta pizza son piminetos y tofu")
+else:
+    print ("los ingredientes para esta pizza son Peperoni, Jamón y Salmón")
