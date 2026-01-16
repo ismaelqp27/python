a2b9bc346b39b724bd4dc2a7a7c75cf6cf8db271
Ismael Quesada Plata <ismaelqp27@gmail.com>
2026-01-16 14:11:58 +0100

    ejercicio6 diccionario

---
+++ ejercicio6.py
@@ -0,0 +1,21 @@
+persona = {}
+
+dato = input("Introduce tu nombre: ")
+persona['Nombre'] = dato
+print("Contenido actual:", persona)
+
+dato = input("Introduce tu edad: ")
+persona['Edad'] = dato
+print("Contenido actual:", persona)
+
+dato = input("Introduce tu sexo: ")
+persona['Sexo'] = dato
+print("Contenido actual:", persona)
+
+dato = input("Introduce tu teléfono: ")
+persona['Teléfono'] = dato
+print("Contenido actual:", persona)
+
+dato = input("Introduce tu correo electrónico: ")
+persona['Email'] = dato
+print("Diccionario finalizado:", persona)
